753bdd577ac049a6e913a7ba338f8aab9547aab3
zenxol <aiden.nguyen101@gmail.com>
2025-06-16 11:13:01 -0400

    real initial commit

--- frontend/app.py
+++ frontend/app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from PIL import Image
 from backend.image_utils import extract_features
 from qdrant_client import QdrantClient
@@ -12,7 +15,7 @@ uploaded_file = st.file_uploader("Upload a dog image", type=["jpg", "jpeg", "png
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     temp_path = "frontend/temp.jpg"
     image.save(temp_path)
@@ -33,6 +36,6 @@ if uploaded_file is not None:
 
         image_path = os.path.join("data/images/Images", breed, filename)
         if os.path.exists(image_path):
-            st.image(image_path, caption=f"{breed} - Score: {score:.4f}", use_column_width=True)    
+            st.image(image_path, caption=f"{breed} - Score: {score:.4f}", use_container_width=True)    
         else:
             st.write(f"{breed} ({filename}) - Score: {score:.4f}")
